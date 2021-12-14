0f7e57b8bba17c71761bf84b3ca5477e71f7d7f6
resh <reshmansiva96@gmail.com>
2021-12-14 01:57:10 +0000

    Refactoring and adding displayTime method

--- src/main/java/com/sparta/reshman/display/DisplayManager.java
+++ src/main/java/com/sparta/reshman/display/DisplayManager.java
@@ -6,22 +6,7 @@ import java.util.Arrays;
 
 public class DisplayManager {
 
-    /*
-    Only deals with printing/displaying
-     */
-
-    public static void requestSorter() {
-        System.out.println("Please select the Sorter method by entering the option number below: ");
-        System.out.println("1) BinarySorter \n" +
-                "2) BubbleSorter \n " +
-                "3) InsertionSorter \n + " +
-                "4) MergeSorter \n + " +
-                "5) QuickSorter");
-    }
-
-    public static void requestArraySize() {
-        System.out.println("Please enter the desired size of the array as a number: ");
-    }
+    //Only deals with printing/displaying
 
     public static void displayPreSort(SorterEnums sortingMethodName) {
         System.out.println("Sorting using " + SorterEnums.toString(sortingMethodName));
@@ -31,4 +16,20 @@ public class DisplayManager {
         System.out.println(Arrays.toString(sortedArray));
     }
 
+    public static void displayTime(long timeTaken) {
+        System.out.println("Time taken: " + timeTaken + " nanoseconds.");
+    }
+
+    public static void requestArraySize() {
+        System.out.println("Please enter the desired size of the array as a number: ");
+    }
+
+    public static void requestSorter() {
+        System.out.println("Please select the Sorter method by entering the option number below: ");
+        System.out.println("1) BinarySorter \n" +
+                "2) BubbleSorter \n " +
+                "3) InsertionSorter \n + " +
+                "4) MergeSorter \n + " +
+                "5) QuickSorter");
+    }
 }
