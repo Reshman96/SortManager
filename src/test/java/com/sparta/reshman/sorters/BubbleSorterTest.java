0386f107aa7bd4df900879f334c5bd72b6a33a23
resh <reshmansiva96@gmail.com>
2021-12-14 16:51:30 +0000

    Tests fixed for working bubblesorter

--- src/test/java/com/sparta/reshman/sorters/BubbleSorterTest.java
+++ src/test/java/com/sparta/reshman/sorters/BubbleSorterTest.java
@@ -1,4 +1,22 @@
 package com.sparta.reshman.sorters;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 public class BubbleSorterTest {
+    private BubbleSorter bubbleSorter;
+
+    @BeforeEach
+    void Setup() {
+        bubbleSorter = new BubbleSorter();
+    }
+
+    @Test
+    void BubbleSortOnAnArrayTest() {
+        Assertions.assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7), bubbleSorter.sort(new int[]{5, 4, 7, 3, 1, 2, 6}));
+        bubbleSorter.timeTaken();
+    }
 }
