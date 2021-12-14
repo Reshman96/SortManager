814329a3f272fc4e1d36bd6c15b6e14ee0290aa4
resh <reshmansiva96@gmail.com>
2021-12-14 01:50:17 +0000

    SortFactory created

--- src/main/java/com/sparta/reshman/start/SortFactory.java
+++ src/main/java/com/sparta/reshman/start/SortFactory.java
@@ -1,4 +1,22 @@
 package com.sparta.reshman.start;
 
+import com.sparta.reshman.enums.SorterEnums;
+import com.sparta.reshman.sorters.*;
+
 public class SortFactory {
+    public static Sorter getSorter(SorterEnums sorter) {
+        switch(sorter) {
+            case BINARY:
+                return new BinarySorter();
+            case BUBBLE:
+                return new BubbleSorter();
+            case INSERTION:
+                return new InsertionSorter();
+            case MERGE:
+                return new MergeSorter();
+            case QUICK:
+                return new QuickSorter();
+        }
+        return null;
+    }
 }
