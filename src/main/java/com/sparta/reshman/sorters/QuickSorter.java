07ad827ad0d15c9f7e565db5b687fddf6268e6b6
resh <reshmansiva96@gmail.com>
2021-12-14 01:53:13 +0000

    Sorters implementing Sorter interface

--- src/main/java/com/sparta/reshman/sorters/QuickSorter.java
+++ src/main/java/com/sparta/reshman/sorters/QuickSorter.java
@@ -1,4 +1,14 @@
-package com.sparta.reshman.sorters.binaryTree;
+package com.sparta.reshman.sorters;
 
-public class QuickSorter {
+public class QuickSorter implements Sorter{
+
+    @Override
+    public int[] sort(int[] generatedArray) {
+        return new int[0];
+    }
+
+    @Override
+    public long timeTaken() {
+        return 0;
+    }
 }
